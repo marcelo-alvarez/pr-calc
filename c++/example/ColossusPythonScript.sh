78a9cf8a8e0567f2d80179e11bd7cb8d26b5557f
Margaret <ikape@astro.utoronto.ca>
2021-09-16 16:38:24 -0700

    adding runscripts

---
+++ c++/example/ColossusPythonScript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# sys.argv
+
+
+import sys
+
+python ../../py/tables/fcoll_zreion.py 3E9 100
---
+++ c++/example/runscript.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+bin=../bin
+nprocs=$1
+
+if [ ! $nprocs > 0 ] ; then
+    echo "nprocs = $nprocs not > 0, exiting"
+    exit
+fi
+
+source ../../scripts/banner.sh
+seed=18937
+pkfile=wmap5_0_m.pk
+
+mybanner "RFAST TEST WITH NPROCS = $nprocs"
+
+mybanner "Testing initial conditions"
+srun -n $nprocs $bin/ics parameterfiles/param.ics -p $pkfile -o delta -b 4e3 -n 2048 -v -s $seed
+
+mybanner "Testing delta2zreion"
+srun -n $nprocs $bin/delta2zreion parameterfiles/param.d2z -f test -m 3e9 -z 50 -r 300 -v
+
+mybanner "Testing all sky map"
+#srun -n $nprocs $bin/allskymap parameterfiles/param.asm -i test -o test -v
+
+mybanner "Testing flat sky map"
+srun -n $nprocs $bin/flatskymap parameterfiles/param.fsm -i test -o test -v
+
+mybanner "Output angular power spectrum to file"
+/global/cscratch1/sd/ikapem/ksz-reionization/pr-calc/scripts/map2pk.py maps/test.kszmap test.ksz_cl.txt
+
+# show the ksz map and output it to a pdf file
+mybanner "Show ksz map"
+/global/cscratch1/sd/ikapem/ksz-reionization/pr-calc/scripts/showmap.py maps/test.kszmap test.kszmap.pdf
+
