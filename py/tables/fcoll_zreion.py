f0d03a9c4b31170789b098d305667c46d748b3ed
Margaret <ikape@astro.utoronto.ca>
2021-09-27 19:54:34 -0700

    new commits aftre revert didn't work

--- py/tables/fcoll_zreion.py
+++ py/tables/fcoll_zreion.py
@@ -3,9 +3,7 @@
 
 # Imports
 
-# In[1]:
-
-
+import sys
 import numpy as np
 from colossus.cosmology import cosmology
 from colossus.lss import peaks
@@ -15,334 +13,164 @@ from scipy.integrate import quad
 from colossus.lss import mass_function
 import matplotlib.pyplot as plt
 
-
 # Variables + cosmology
 
-# In[2]:
-
-
 #we set our own cosmology
-params        = {'flat': True, 'H0': 69.0, 'Om0': 0.282, 'Ob0': 0.042, 'sigma8': 0.81, 'ns': 0.96}  #Check this with pr-calc
-cosmo         = cosmology.setCosmology('myCosmo', params)
-
-h             = cosmo.H0 / 100.
+#params         = {'flat': True, 'H0': 69.0, 'Om0': 0.2863, 'Ob0': 0.0463, 'sigma8': 0.82, 'ns': 0.96}  #Check this with pr-calc
+params         = {'flat': True, 'H0': 70.0, 'Om0': 0.27, 'Ob0': 0.044, 'sigma8': 0.8, 'ns': 0.96}
+cosmo          = cosmology.setCosmology('myCosmo', params)
+h              = cosmo.H0 / 100.
 
 # Mmin and zeta
-Mmin          = 3E9 / h # in Msun
-Mmax          = 1E15/ h  #1E12/ h
-zeta          = 50
+#Mmin in Msun (M in Colossus is in Msun/h) --------- This doesn't make M the same as in pr-calc anymore
+Mmin           = float(sys.argv[1]) / h
+Mmax           = 1E15/ h    
+zeta           = float(sys.argv[2])
 
 # create grid of delta_R and R and the zreion table
 # can mess with the grid later but an initial guess follows
-nR            = 10
-ndelta_R      = 50
-Rvalsmin      = 1.0
-Rvalsmax      = 1e3
-delta_Rmin    = -2
-delta_Rmax    =  2
-Rvals         = np.logspace(np.log10(Rvalsmin), np.log10(Rvalsmax), nR)
-delta_Rvals   = np.linspace(delta_Rmin, delta_Rmax, ndelta_R)
-zreion_table  = np.zeros((nR, ndelta_R))
+nR             = 32
+ndelta_R       = 33 # odd ndelta_R for a delta_R=0 entry with symmetric range
+Rvalsmin       = 1.0
+Rvalsmax       = 1e3
+delta_Rmin     = -1
+delta_Rmax     =  1
+Rvals          = np.logspace(np.log10(Rvalsmin), np.log10(Rvalsmax), nR)
+delta_Rvals    = np.linspace(delta_Rmin, delta_Rmax, ndelta_R)
+zreion_table   = np.zeros((nR, ndelta_R))
 
 # create grid of z and the fcoll table
-dz            = 0.1
-zmin          = 0.0 # just for completeness
-zmax          = 20  # most reionization models we don't care about z>20 
-nz            = int((zmax - zmin) / dz)
-fcoll_table   = np.zeros(nz)
+dz             = 0.5   
+zmin           = 0.0 # just for completeness
+zmax           = 20  # most reionization models we don't care about z>20 
+nz             = int((zmax - zmin) / dz)
+fcoll_table    = np.zeros(nz)
 
-rho_Mpc       = 2.775E11 * cosmo.Om0 * h**2
+rho_Mpc        = 2.775E11 * cosmo.Om0 * h**2
  
-zvals         = np.linspace(zmin, zmax, nz) # zvals will be approximately spaced at dz
-fcoll_table   = np.zeros(nz)
+zvals          = np.linspace(zmin, zmax, nz)  # zvals will be approximately spaced at dz
+fcoll_table    = np.zeros((nz, nR, ndelta_R)) # holds all the collapsed fractions
+
+uncond_fcoll   = np.zeros(len(zvals))
+uncond_fcollPS = np.zeros(len(zvals))
+
+# meshgrids for independent variables
+Rvals2d, delta_Rvals2d          = np.meshgrid(Rvals,   delta_Rvals, indexing='ij')
+zvals3d, Rvals3d, delta_Rvals3d = np.meshgrid(zvals,   Rvals,   delta_Rvals, indexing='ij')
 
 # convert Mmin to Rmin
-Rmin          = (3*Mmin/4/np.pi/rho_Mpc)**(1./3.)
+Rmin_Mpc       = (3*Mmin/4/np.pi/rho_Mpc)**(1./3.)   #R=(3M/4 pi)**1/3
+Rmin           = Rmin_Mpc*h # Rmin is used by colussus and is in Mpc/h (multiply by )
 
-#print(Rvals)
-#print(zvals)
-print(delta_Rvals)
+dRzero_index   = int((ndelta_R+1)/2)-1
 
+# hmf and halo mass definition
+hmf_mdef       = 'fof'
+hmf_model      = 'press74'
+#hmf_mdef      = 'fof'
+#hmf_model     = 'sheth99'
+#hmf_mdef      = '200m'
+#hmf_model     = 'tinker08'
 
-# In[3]:
-
-
-#print(zvals)
-#print(delta_Rvals)
-#print(Rvals)
-
-print(len(zvals))
-print(len(delta_Rvals))
-print(len(Rvals))
+# plotting 
+plt.rcParams['figure.figsize'] = [12, 8]
 
 
 # Function to compute PS conditional fcoll
 
-# In[4]:
-
-
-# function for fcoll conditional
-def cond_fcoll(z, delta_R, R):
-    delta_c   = peaks.collapseOverdensity(corrections=True,z=z)
-    #sigma_min = cosmo.sigma(Rmin,z)
-    sigma_R   = cosmo.sigma(R,z)
+# function for fcoll = PS conditional collapsed fraction (ccfPS)
+def ccfPS(z, R, delta_R):
+    delta_c   = peaks.collapseOverdensity(corrections = True,z = z)
+    sigma_R   = cosmo.sigma(R, z)  #R in Mpc/h
+    sigma_min = cosmo.sigma(Rmin, z) #Rmin in Mpc/h
     arg       = ((delta_c - delta_R) / (np.sqrt(2.*sigma_min**2 - sigma_R**2)))
     f         = erfc(arg)
     return f
-
-cond_fcollPS = np.zeros((len(zvals), len(Rvals), len(delta_Rvals)))
-for i in range(len(zvals)):
-    #print(i,nz)
-    sigma_min    = cosmo.sigma(Rmin,zvals[i])
-    delta_c      = peaks.collapseOverdensity(corrections = True, z = zvals[i])
-    for j in range(len(Rvals)):
-        sigma_R      = cosmo.sigma(Rvals[j], zvals[i], j=0, filt='sharp-k', inverse=False, derivative=False, kmin=None, kmax=None, ps_args={'model': 'eisenstein98', 'path': None})
-        for k in range(len(delta_Rvals)): 
-            cond_fcollPS[i,j,k] = cond_fcoll(zvals[i], delta_Rvals[k], Rvals[j])
+ccfPS = np.vectorize(ccfPS)
 
 
-# In[5]:
+# Function to compute unconditional fcoll from colossus for a given hmf model and mass definition
 
-
-#print(cond_fcollPS)
-#print(cond_fcollPS[:,:,:])#same as above, all of fcoll corresponding to all of zvals, rvals and delta_Rvals
-#print(cond_fcollPS[0,:,:])# prints fccoll at z=0, for all of rvals and deltarvals. zvalls index goes from 0 t0 199 (because it's len =200) which corresponds to z 20
-#print(cond_fcollPS[:,0,:])#prints out all the fccoll at the first value of rvals (in this casee 1), for all values of zvals and deltarvals. index goes to 10 which correspnds to rvals  1000
-print(cond_fcollPS[:,:,0])#prints out all the fcoll at the first value of deltarvals (in this case -2), for all values of zvals and rvals. index goes to 10 which corresponds to deltarvals of 2
-print(cond_fcollPS[:,:,0].shape)
-
-
-# In[6]:
-
-
-plt.plot(zvals, cond_fcollPS[:,9,9], 'r')
-#plt.plot(cond_fcollPS[20,:,9], cond_fcollPS[0,0,:], 'r--')
-plt.plot(zvals, cond_fcollPS[:,-5,5], 'b')
-plt.plot(zvals, cond_fcollPS[:,4,7], '--g')
-plt.yscale('log')
-
-plt.xlabel('z', fontsize=20)
-plt.ylabel('F_coll', fontsize=20)
-plt.tick_params(labelsize=20)
-plt.legend(loc='best', fontsize=10)
-plt.show()
-
-print(cond_fcollPS[20,:,9])  #Rvals
-
-
-# making different plots to further analayse the data
-# 
-
-# In[7]:
-
-
-
-plt.plot(zvals, cond_fcollPS[:,0,0], '--g')
-plt.plot(zvals, cond_fcollPS[:,0,5], 'r')
-
-plt.yscale('log')
-plt.show()
-
-
-# In[8]:
-
-
-#plot fcoll at several different values of z, 1) vs R at a few values of delta_R, and 2) vs delta_R at a few values of R
-plt.plot(cond_fcollPS[:,0,0])
-plt.show()
-
-
-# compute unconditional fcoll from colossus. Ideally, this would be done for PS nad ST
-
-# In[43]:
-
-
-def intergrand_fcoll(M, rho_Mpc, redshift, model):
-    
+def integrand(lnM, rho_Mpc, redshift, model, mdef):
     '''
-    This function computes the collapse fraction as an inteegral of the mass function indicated by the user
+    This function computes the collapse fraction as an integral of the mass function indicated by the user
     '''
+    h  = cosmo.H0 / 100. 
+    M  = np.exp(lnM)     # input mass assumed to be in Msun
+    M *= h               # convert from Msun to Msun/h for Colussus
     
-    for model in mass_function.models:
-        if 'fof' in mass_function.models[model].mdefs:
-            mfunc  = mass_function.massFunction(M, redshift, model = model, mdef = 'fof', q_out = 'dndlnM')
-        else:
-            mfunc = mass_function.massFunction(M, redshift, mdef = '200m', model = model)
-            
-            M     /= 0.69
-            mfunc  = mfunc * (1/M)   #convert dndlnM to dndM
-            
-            mfunc *= 0.69**3
-            return (1/rho_Mpc) * mfunc * M 
-        if M > (1e13 / 0.69):
-            return 0
+    dndlnM = mass_function.massFunction(M, redshift, mdef = mdef, model = model, q_out = 'dndlnM')
+
+    dndlnM *= h**3 # convert mass function from 1/(Mpc/h)^3 to 1/Msun/Mpc^3
+    M      /= h    # convert mass back to Msun from Msun/h 
     
+    dfcolldlnM = 1/rho_Mpc * M * dndlnM   # convert dfcoll/dlnM = 1/rho*M*dn/dlnM
 
-F_coll = np.zeros((len(zvals), len(Rvals), len(delta_Rvals)))
-F_coll_PS = np.zeros((len(zvals), len(Rvals), len(delta_Rvals)))
+    return dfcolldlnM
 
-#print(F_coll)
-
-for i in range(len(zvals)):
-    F_coll[i,0,0], err = quad(intergrand_fcoll, Mmin, Mmax, args=(rho_Mpc, zvals[i], 'press74'), epsabs = 1e-6, limit=100)
-    F_coll_PS[i,0,0], err = quad(intergrand_fcoll, Mmin, Mmax, args=(rho_Mpc, zvals[i], 'press74'), epsabs = 1e-6, limit=100)
-    
-
-
-# In[44]:
+def ucf(z,hmf_model,hmf_mdef):
+    # integrate hmf to obtain collapsed fraction 
+    from scipy import integrate
+    uncond_fcoll,   err = quad(integrand, np.log(Mmin), np.log(Mmax), 
+                                args=(rho_Mpc, z, hmf_model, hmf_mdef))
+    return uncond_fcoll
+ucf = np.vectorize(ucf)
 
 
-print(F_coll[:,0,0])
-#print(F_coll_PS)
-print(1e-20)
+# Function to compute conditional fcoll using equation 3 from overleaf
+
+def ccf(z, R, delta_R, ucf_func, ucfPS_func):
+    # eqn 3 from overleaf 
+    # cond_fcoll(z,R,delta_R) = uncond_fcoll(z) / uncond_fcollPS * cond_fcollPS
+    return ucf_func(z) / ucfPS_func(z) * ccfPS(z, R, delta_R) 
 
 
-# check that uunconditional fcoll aat a given reedshift is equal to conditionaal fcoll when R is large and dR ~ 0
+# Function to compute reionization redshift at a given radius and density contrast
 
-# In[45]:
-
-
-#check that uncond_fcoll aat a given z == cond_fcoll when r is large and dR ~0
-plt.plot(zvals, cond_fcollPS[:,9,27], 'r:', label='conditional at R=1000, and deltaR ~0') # zvals, rvals,deltarvals
-#plt.plot(zvals, cond_fcollPS[:,1,13], 'r', label='conditional at R=1000, and deltaR ~0') # zvals, rvals,deltarvals
-plt.plot(zvals, F_coll_PS[:,0,0], '-', label = 'unconditional') #
-
-plt.xlabel('z', fontsize=20)
-plt.ylabel('F_coll', fontsize=20)
-plt.tick_params(labelsize=20)
-plt.legend(loc='best', fontsize=10)
-plt.yscale('log')
-plt.axhline(1, linestyle='--')
-plt.legend()
-plt.show()
-
-#red dotted line should match blue curve
+def zreion(R, delta_R, ucf_func, ucfPS_func, zeta):
+    # note zvals is a global variable!!!
+    fcoll_zvals = ccf(zvals,R,delta_R,ucf_func, ucfPS_func)
+    zeta_times_fcoll_zvals = zeta * fcoll_zvals
+    z_of_zeta_times_fcoll = interp1d(zeta_times_fcoll_zvals, zvals, fill_value="extrapolate")
+    zreion = z_of_zeta_times_fcoll(1.0)
+    return zreion
+zreion = np.vectorize(zreion)
 
 
-# compute the final conditional fcoll from equation 3 in overleeaf
-
-# In[42]:
-
-
-
-print(F_coll.shape)
-print(cond_fcollPS.shape)
-
-#F_coll_final = 
-
-F_coll = np.nan_to_num(F_coll)
-F_coll_PS = np.nan_to_num(F_coll_PS)
-cond_fcollPS[:,:,:] = np.nan_to_num(cond_fcollPS[:,:,:])
-
-F_coll_final = (F_coll * cond_fcollPS[:,:,:]) / F_coll_PS #eqn 3...F_coll_PS[i] = bar(fcollPS), = fcollPS
-print(F_coll_final)
-
-#print(F_coll_final.shape)
-#print(F_coll_PS[1,:,:])
+# Populate collapsed fraction tables and set up collapsed fraction interpolation tables
+# tables for fcoll(z)
+#   these are the unconditional collapsed fractions for PS 
+#   as well as that for the specified hmf model
+uncond_fcoll   = ucf(zvals,hmf_model,hmf_mdef).astype(np.float32)
+uncond_fcollPS = ucf(zvals,'press74','fof').astype(np.float32)
+ucf_interp     = interp1d(zvals, uncond_fcoll)
+ucfPS_interp   = interp1d(zvals, uncond_fcollPS)
 
 
-# plot the values to be sure they agree since we are begining with just PS for checks
+# Populate zreion table
 
-# In[18]:
+# table for zreion(R,delta_R)
+#   this is the reionization redshift at a given overdensity and scale 
+#   from the condition
+#   zeta * fcoll(R,delta_R,zreion) = 1
+zreion_table = zreion(Rvals2d, delta_Rvals2d, ucf_interp, ucfPS_interp, zeta).astype(np.float32)
 
+# Write tables in binaries with minimal headers easily readable in C
 
-plt.plot(zvals, (F_coll_PS[:,0,0]), 'b-', label = 'unconditional from colosus') #
-
-
-plt.semilogy(zvals,F_coll_final[:,0,0], 'r-', label='Final conditional fcoll from equation')
-plt.xlabel('z', fontsize=20)
-plt.ylabel('F_coll', fontsize=20)
-    
-plt.plot(zvals,cond_fcollPS[:,-1,5], 'g:', label='conditional fcoll PS')
-plt.yscale('log')
-plt.legend()
-plt.show()
-
-#red curve should be equal to greeen
-
-
-# In[ ]:
-
-
-# populate the tables
-fcoll_table = F_coll_final
-#print(fcoll_table)
-
-
-# In[ ]:
-
-
-for i in range(len(Rvals)):
-    for j in range(len(delta_Rvals)):
-        cond_fcoll_zvals = cond_fcoll(zvals, delta_Rvals[j], Rvals[i])
-        zeta_times_fcoll_zvals = zeta * cond_fcoll_zvals
-        z_of_zeta_times_fcoll = interp1d(zeta_times_fcoll_zvals, zvals, fill_value="extrapolate")
-        zreion_table[i,j] = z_of_zeta_times_fcoll(1.0)
-zreion_table = zreion_table.astype(np.float32)
-
-
-# In[ ]:
-
-
-print(len(zvals))
-print(len(F_coll_final))
-print(len(Rvals))
-
-
-# In[ ]:
-
-
-zreion_table = np.nan_to_num(zreion_table)
-print(zreion_table[:,49])#all values of zreion at rvals index 2
-print(zreion_table.shape)
-
-
-# In[ ]:
-
-
-#plot z_reion 1) vs R at a few values of delta_R, and 2) vs delta_R at a few values of R
-plt.plot(Rvals, zreion_table[:,49], 'o')
-plt.yscale('log')
-plt.show( )
-
-
-# In[ ]:
-
-
-# write the tables in binaries with minimal headers easily readable
-# in C
-# fcoll table
-#wrote tables in .txt as well so I can see the output
-
-tablefile2='fcoll_Mmin'+str(Mmin)+'.txt'  #add the mass function model to the file name
-#tablefile2='fcoll_Mmin'+str(Mmin)+str(mass_function.models[model])+'.txt'
-f2 = open(tablefile2,'w')
-fcoll_table.tofile(f2, sep=" ", format="%f")
-np.savetxt(tablefile2, np.column_stack([zvals, fcoll_table[:,0,0]]), fmt='%.18g')#zvals, Rvals, delta_Rvals, fcoll_table
-
-tablefile='fcoll_Mmin'+str(Mmin)+'.tab'
-f = open(tablefile,'wb')
-zrange = [zmin, zmax]
-zrange = np.asarray(zrange).astype(np.float32)
-dim   = [nz]
-dim   = np.asarray(dim).astype(np.int32)
+# fcoll(z) (unconditional mass function)  ......why writing the unconditional to file?
+fcoll_tablefile='fcoll_table.tab'
+f = open(fcoll_tablefile,'wb')
+zrange       = np.asarray([zvals[0],zvals[-1]]).astype(np.float32)
+dim          = np.asarray([len(zvals)]).astype(np.int32)
+fcoll_table  = uncond_fcoll.astype(np.float32)
 zrange.tofile(f)
 dim.tofile(f)
 fcoll_table.tofile(f)
+f.close()
 
-
-# In[ ]:
-
-
-# zreion table
-tablefile3='zreion_zeta'+str(zeta)+'_Mmin'+str(Mmin)+'.txt'
-#print(tablefile)
-f3 = open(tablefile3,'w')
-zreion_table.tofile(f3, sep=" ", format="%f")
-np.savetxt(tablefile3, np.column_stack([Rvals, zreion_table]), fmt='%.18g')
-
-tablefile='zreion_zeta'+str(zeta)+'_Mmin'+str(Mmin)+'.tab'
-f = open(tablefile,'wb')
+# zreion(R,delta_R) (reionization redshifts) 
+zreion_tablefile='zreion_table.tab'
+f = open(zreion_tablefile,'wb')
 ranges = [Rvalsmin,Rvalsmax,delta_Rmin,delta_Rmax]
 ranges = np.asarray(ranges).astype(np.float32)
 dims   = [nR, ndelta_R]
@@ -350,73 +178,73 @@ dims   = np.asarray(dims).astype(np.int32)
 ranges.tofile(f)
 dims.tofile(f)
 zreion_table.tofile(f)
-
-
-# In[ ]:
-
-
 f.close()
-f2.close()
-f3.close()
 
 
-# In[ ]:
+# Done with table generation. Functions below are for reading tables and testing results.
 
+# function to read zreion table
+def read_zreiontable(fname):
+    f = open(fname,'rb')  #fname has to be defined somehow so it knows what file to open, right?
+    Rrange       = np.fromfile(f,dtype=np.float32, count=2)
+    deltaRrange  = np.fromfile(f,dtype=np.float32, count=2)
+    dims         = np.fromfile(f,dtype=np.int32,   count=2)
+    zreion_table = np.fromfile(f,dtype=np.float32, count=dims[0]*dims[1])
+    zreion_table = np.reshape(zreion_table,(dims[0],dims[1]))
+    print('read table of dimensions',dims)
+    return Rrange, deltaRrange, dims, zreion_table
 
+# check table read and zreion
+def plot_zreion_from_table(fname):
+    #plot z_reion 1) vs R at a few values of delta_R, and 2) vs delta_R at a few values of R
+    Rrange, deltaRrange, dims, zreion_table = read_zreiontable(fname)
+    Rvals      = np.logspace(np.log10(Rrange[0]),     np.log10(Rrange[1]),     dims[0])
+    deltaRvals = np.logspace(         deltaRrange[0],          deltaRrange[1], dims[1])
+    plt.plot(Rvals, zreion_table[:,0]           ,'r:',label='$\delta_R=$'+str(delta_Rvals[0]))
+    plt.plot(Rvals, zreion_table[:,dRzero_index],'k-',label='$\delta_R=$'+str(delta_Rvals[dRzero_index]))
+    plt.plot(Rvals, zreion_table[:,-1]          ,'b:',label='$\delta_R=$'+str(delta_Rvals[-1]))
 
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-def intergrand_fcoll(M, rho_Mpc, redshift):
-    mfunc  = mass_function.massFunction(M, redshift, mdef = 'fof', model = 'press74', q_out = 'dndlnM')
-    mfunc  = mfunc * (1/M)   #convert dndlnM to dndM
-    M     /= 0.69
-    mfunc *= 0.69**3
-    return (1/rho_Mpc) * mfunc * M    
-
-F_coll = np.zeros(len(zvals))
-F_coll_PS = np.zeros(len(zvals))
-
-for i in range(len(zvals)):
-    F_coll[i], err = quad(intergrand_fcoll, Mmin, Mmax, args=(rho_Mpc, zvals[i]))
-    F_coll_PS[i], err = quad(intergrand_fcoll, Mmin, Mmax, args=(rho_Mpc, zvals[i]))
+    plt.legend(fontsize=20)
+    plt.tick_params(labelsize=20)
+    plt.xscale('log')
+    plt.xlabel('R [Mpc]',fontsize=20)
+    plt.ylabel(r'$z_{\rm reion}$',fontsize=20)
+    plt.savefig('zreion.png',bbox_inches='tight')
+    #plt.show()
+    print('zreion should increase towards higher R and delta_R')
+    print('this is not an exhaustive test, just a single quick check')
     
+def show_zreion_from_table(fname):
+    #plot z_reion 1) vs R at a few values of delta_R, and 2) vs delta_R at a few values of R
+    Rrange, deltaRrange, dims, zreion_table = read_zreiontable(fname)
+    Rvals      = np.logspace(np.log10(Rrange[0]),     np.log10(Rrange[1]),     dims[0])
+    deltaRvals = np.logspace(         deltaRrange[0],          deltaRrange[1], dims[1])
+    
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+    ax=plt.gca()
+    im=ax.imshow(np.transpose(zreion_table),interpolation='bilinear',
+               origin='lower',extent=[np.log10(Rrange[0]),np.log10(Rrange[1]),deltaRrange[0],deltaRrange[1]])
+    plt.xlabel(r'$\log\ R$ [Mpc]',fontsize=20)
+    plt.ylabel(r'$\delta_{R}$',fontsize=20)
+
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cb=plt.colorbar(im,cax=cax)
+    cb.set_label(r'$z_{\rm reion}$',fontsize=20)
+    _cont = ax.contour(np.transpose(zreion_table),levels=np.linspace(4,20, 10), colors='white', alpha=0.5,
+               origin='lower',extent=[np.log10(Rrange[0]),np.log10(Rrange[1]),deltaRrange[0],deltaRrange[1]])
+    cb.add_lines(_cont)
+    #plt.show()
+    plt.savefig('zreion2.png',bbox_inches='tight')
+    print('zreion should increase towards higher R and delta_R')
+    print('this is not an exhaustive test, just a single quick check')
+
+#I don't think we neeed the plots in here???
+#added them again for test
+
+#Look at zreion table
+
+#plot_zreion_from_table(zreion_tablefile)
+
+#show_zreion_from_table(zreion_tablefile)
