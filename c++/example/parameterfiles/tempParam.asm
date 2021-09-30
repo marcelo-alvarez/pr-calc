65263d047dccd8e824032b4dcf9b3c5d779b011b
Renee Hlozek <renee.hlozek@gmail.com>
2021-09-30 11:35:40 -0700

    added temporary param files that will generate the actual files you use

---
+++ c++/example/parameterfiles/tempParam.asm
@@ -0,0 +1,37 @@
+# Cosmological parameters
+Omegam        XX
+Omegal        XX
+Omegab        XX
+h             XX
+ns            XX
+sigma8        XX
+w             XX
+	
+# Box size, dimension, and redshift of input density field
+
+BoxSize         XX
+N               XX
+zInit           XX
+# This is the Reionization redshift file location.
+# The file should be the reionization field
+
+DeltaFile	./ICs/delta
+
+# These are the parameters describing the map of tau
+
+fov	    	XX
+	# field of view in degrees
+theta		XX
+	# polar angle of center of map in degrees
+phi		XX
+# azimuthal angle of center of map in degrees
+NPixels		XX
+# number of pixels per dimension in map
+NSide		XX
+# Healpix N_side parameter
+
+# The parameters are for the actual raytracing
+
+NRedshifts       XX
+InitialRedshift  XX
+FinalRedshift    XX
