9754fa4f6766aff356a4ecab31b669d45b9f718d
Margaret <ikape@astro.utoronto.ca>
2021-09-29 19:48:17 -0700

    aadded setCosmology filee

---
+++ c++/example/setCosmology.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#we set our own cosmology
+from colossus.cosmology import cosmology
+def param_names():
+    params         = {'flat': True, 'H0': 70.0, 'Om0': 0.27, 'Ob0': 0.044, 'sigma8': 0.8, 'ns': 0.96}
+    cosmo          = cosmology.setCosmology('myCosmo', params)
+    h              = cosmo.H0 / 100.
+    return cosmo
+
+#print(h)
+
