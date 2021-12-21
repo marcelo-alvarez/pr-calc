3db7355255dd99c47209d9852979a242fec3fd84
Marcelo Alvarez <marcelo.alvarez@berkeley.edu>
2021-12-21 10:40:21 -0800

    fix runexample

--- example/runexample.sh
+++ example/runexample.sh
@@ -15,13 +15,15 @@ pkfile=wmap5_0_m.pk
 mybanner "RFAST TEST WITH NPROCS = $nprocs"
 
 mybanner "Testing initial conditions"
-srun -n $nprocs $bin/ics parameterfiles/param.ics -p $pkfile -o delta -b 8e3 -n 512 -v -s seed
+srun -n $nprocs $bin/ics parameterfiles/param.ics -p $pkfile -o delta -b 8e3 -n 512 -v -s $seed
 
 mybanner "Testing delta2zreion"
 srun -n $nprocs $bin/delta2zreion parameterfiles/param.d2z -f test -m 1e9 -z 500 -r 32 -v
 
 mybanner "Testing all sky map"
-#srun -np $nprocs $bin/allskymap parameterfiles/param.asm -i test -o test -v
+#srun -n $nprocs $bin/allskymap parameterfiles/param.asm -i test -o test -v
 
 mybanner "Testing flat sky map"
 srun -n $nprocs $bin/flatskymap parameterfiles/param.fsm -i test -o test -v
+
+
