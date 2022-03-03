62850b4bb22d35ae03610935e3aca77359655f4c
Marcelo Alvarez <marcelo.alvarez@berkeley.edu>
2022-03-03 12:38:08 -0800

    clean up automake-based installation

---
+++ c++/clean_all.sh
@@ -0,0 +1,3 @@
+rm -rf bin
+if [ -e "Makefile" ] ; then make clean ; fi
+source clean_automake.sh
---
+++ c++/clean_automake.sh
@@ -0,0 +1,24 @@
+# clean automake
+rm -rf \
+   aclocal.m4 \
+   autom4te.cache \
+   autoscan.log \
+   config.status \
+   configure \
+   config.log \
+   config.h \
+   depcomp \
+   install-sh \
+   INSTALL \
+   missing \
+   Makefile \
+   Makefile.in \
+   compile \
+   config.log \
+   config.status \
+   scripts/Makefile \
+   scripts/Makefile.in \
+   src/.deps/ \
+   src/Makefile \
+   src/Makefile.in \
+   stamp-h1
